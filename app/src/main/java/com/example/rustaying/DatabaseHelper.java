b5c84f063476a61d6ff5b9d8f7457104c6946902
Mohammed Sapin <sapin.mohammed@gmail.com>
2019-03-12 13:38:51 -0400

    fixed errors

--- app/src/main/java/com/example/rustaying/DatabaseHelper.java
+++ app/src/main/java/com/example/rustaying/DatabaseHelper.java
@@ -22,12 +22,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-<<<<<<< HEAD
-        db.execSQL("CREATE TABLE registeruser (ID INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, password TEXT, firstName TEXT, lastName TEXT)");
-        //Change table name to usersTable
-=======
         db.execSQL("CREATE TABLE usersTable (ID INTEGER PRIMARY KEY AUTOINCREMENT, firstName TEXT, lastName TEXT, email TEXT, password TEXT)");
->>>>>>> dcbe4a56472b73f93591527a53d6e855c6be548f
     }
 
     @Override
@@ -40,17 +35,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
 
-<<<<<<< HEAD
-        contentValues.put("email", email);
-        contentValues.put("password", password);
-        //contentValues.put("firstName", firstName);
-        //contentValues.put("lastName", lastName);
-=======
         contentValues.put(COL_2, firstName);
         contentValues.put(COL_3, lastName);
         contentValues.put(COL_4, email);
         contentValues.put(COL_5, password);
->>>>>>> dcbe4a56472b73f93591527a53d6e855c6be548f
 
         //insert() returns the rowId the new inserted item
         long tableInput = db.insert(TABLE_NAME,null,contentValues);
@@ -62,13 +50,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         String[] id = {COL_1};
         SQLiteDatabase db = getReadableDatabase();
 
-<<<<<<< HEAD
-        //3 lines below don't make sense to me
-        //Why do you have account and fullAccount variables? Why are you storing it in the database like that?
-        String account = COL_2 + "=?" + " and " + COL_3 + "=?";
-=======
         String account = COL_4 + "=?" + " and " + COL_5 + "=?";
->>>>>>> dcbe4a56472b73f93591527a53d6e855c6be548f
         String[] fullAccount = {email, password};
         Cursor cursor = db.query(TABLE_NAME,id,account,fullAccount,null,null,null);
         int count = cursor.getCount();
@@ -81,22 +63,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             return false;
         }
     }
-
-    public Guest getGuestInfo(String email)
-    {
-        //Create Guest object and send it back
-
-        SQLiteDatabase db = getReadableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM registeruser;", null);
-        cursor.moveToFirst();
-
-        int id = cursor.getInt(cursor.getColumnIndex("ID"));
-        String password = cursor.getString(cursor.getColumnIndex("password"));
-
-        Guest g = new Guest(id, email, password);
-        return g;
-    }
-
     public boolean isEmpty(EditText text){
         if (text.getText().toString().trim().length() > 0){
             return false;
